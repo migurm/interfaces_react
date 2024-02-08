a9402127a80b05d9c11d473d645418dad19944c3
Miguel Rueda <migue.rueda.90@gmail.com>
2024-02-08 16:18:42 +0100

    Cambio de ordenador

--- ultima_practica/src/App.js
+++ ultima_practica/src/App.js
@@ -1,26 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import './estilos.css';
+import {Routes, Route, NavLink, BrowserRouter, Navigate} from 'react-router-dom';
+
+
+import {Footer} from './componentes/Footer';
+import { Articulos } from './componentes/Articulos';
+
 
 function App() {
   return (
     <>
     <header id="header">
-            <div className="wrap_cabecera">
-                <div id="logo"> 
-                    <span className="gear">S</span>
-                    <h3><a href="blog.html">BLOG</a></h3>
-                </div>
-                <nav id="menu">
-                    <ul>
-                        <li><a href="#">INICIO</a></li>
-                        <li><a href="blog.html" >BLOG</a></li>
-                        <li><a href="#">FORMACIÓN</a></li>
-                        <li><a href="#">CONTACTO</a></li>
-                    </ul>
-                </nav>
-            </div>
-    </header>
+        <div className="wrap_cabecera">
+          <div id="logo"> 
+            <span className="gear">S</span>
+            <h3><a href="blog.html">BLOG</a></h3>
+          </div>
+          <nav id="menu">
+            <ul>
+              <li><NavLink to='/'>INICIO</NavLink></li>
+              <li><NavLink to='/' >BLOG</NavLink></li>
+              <li><NavLink to='/'>FORMACIÓN</NavLink></li>
+              <li><NavLink to='/'>CONTACTO</NavLink></li>
+            </ul>
+          </nav>
+        </div>
+	  </header>
     <main className="wrap">
       <section id="info">
       <div id="banner">
@@ -113,106 +119,13 @@ function App() {
 
           </div>
         </aside>
-        <section id="articles">
-          <h2>Últimos artículos</h2>
-          <div className="clearfix"></div>
-          <article>
-              <div className="datos">
-                  <span>Fecha: 01 de enero</span>
-                  <span>Categoría: Pruebas</span>
-              </div>
-              <h4>
-                  <a href="#">Título de artículo 1</a>
-              </h4>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, dolores. Consequatur quis tenetur, modi quod quam repellat facilis minus molestias dicta eum ex voluptatibus itaque necessitatibus, possimus quas? Fugiat, veritatis!</p>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, dolores. Consequatur quis tenetur, modi quod quam repellat facilis minus molestias dicta eum ex voluptatibus itaque necessitatibus, possimus quas? Fugiat, veritatis!</p>
-          </article>
-          <article>
-              <div className="datos">
-                  <span>Fecha: 01 de febrero</span>
-                  <span>Categoría: Pruebas</span>
-              </div>
-              <h4>
-                  <a href="#">Título de artículo 2</a>
-              </h4>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, dolores. Consequatur quis tenetur, modi quod quam repellat facilis minus molestias dicta eum ex voluptatibus itaque necessitatibus, possimus quas? Fugiat, veritatis!</p>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, dolores. Consequatur quis tenetur, modi quod quam repellat facilis minus molestias dicta eum ex voluptatibus itaque necessitatibus, possimus quas? Fugiat, veritatis!</p>
-          </article>
-          <article>
-              <div className="datos">
-                  <span>Fecha: 01 de marzo</span>
-                  <span>Categoría: Pruebas</span>
-              </div>
-              <h4>
-                  <a href="#">Título de artículo 3</a>
-              </h4>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, dolores. Consequatur quis tenetur, modi quod quam repellat facilis minus molestias dicta eum ex voluptatibus itaque necessitatibus, possimus quas? Fugiat, veritatis!</p>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, dolores. Consequatur quis tenetur, modi quod quam repellat facilis minus molestias dicta eum ex voluptatibus itaque necessitatibus, possimus quas? Fugiat, veritatis!</p>
-          </article>
-          <article>
-              <div className="datos">
-                  <span>Fecha: 01 de abril</span>
-                  <span>Categoría: Pruebas</span>
-              </div>
-              <h4>
-                  <a href="#">Título de artículo 4</a>
-              </h4>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, dolores. Consequatur quis tenetur, modi quod quam repellat facilis minus molestias dicta eum ex voluptatibus itaque necessitatibus, possimus quas? Fugiat, veritatis!</p>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, dolores. Consequatur quis tenetur, modi quod quam repellat facilis minus molestias dicta eum ex voluptatibus itaque necessitatibus, possimus quas? Fugiat, veritatis!</p>
-          </article>
-          <article id="blog">
-              <a href="blog.html">Ir al blog</a>
-          </article>
-        </section>
+      
+        <Articulos></Articulos>
       </div>
-      
-      
     </main>
     
 
-    <footer id="footer">
-        <div className="wrap">
-            <div id="menuFooter">
-                <h5>MENÚ</h5>
-                <ul>
-                    <li><a href="#">INICIO</a></li>
-                    <li><a href="blog.html">BLOG</a></li>
-                    <li><a href="#">FORMACIÓN</a></li>
-                    <li><a href="#">CONTACTO</a></li>
-                </ul>
-            </div>
-            <div id="location">
-                <h5>DÓNDE ESTAMOS?</h5>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3199.2996623515555!2d-4.462811723546774!3d36.69133637227841!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f9c45623d2c3%3A0x7a634bd28c0cf953!2sMEDAC%20Nova%20%F0%9F%A5%87%20Centro%20Tecnol%C3%B3gico%20de%20FP!5e0!3m2!1ses!2ses!4v1698701252334!5m2!1ses!2ses" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
-            <div id="info">
-                <h5>Desarrollado con</h5>
-                <p>
-                    <img src="img/html5-badge-h-css3-graphics-multimedia-performance-semantics.png" alt="Creado con HTML y CSS"/>
-                </p>
-                <h5>Optimizado para</h5>
-                <p id="browsers">
-                    <a href="#">
-                        <img src="img/firefox-icon.png" alt="Firefox"/>
-                    </a>
-                    <a href="#">
-                        <img src="img/chrome.png" alt="Chrome"/>
-                    </a>
-                    <a href="#">
-                        <img src="img/safari.png" alt="Safari"/>
-                    </a>
-                    <a href="#">
-                        <img src="img/android.png" alt="Android"/>
-                    </a>
-                    <a href="#">
-                        <img src="img/Opera_256x256.png" alt="Opera"/>
-                    </a>
-                </p>
-                <h5>AUTOR</h5>
-                <p> MIGUEL RUEDA - 2º WEB</p>
-            </div>
-        </div>
-    </footer>
+    <Footer></Footer>
 
     </>
   );
